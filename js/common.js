70610c010071933ba6d5f500c09d017b0de798fa
nupigm <nupigm@outlook.com>
2025-03-21 22:54:51 +0800

    2025-03-21

--- js/common.js
+++ js/common.js
@@ -23,23 +23,74 @@ function loaded() {
       console.log("<footer>标签是由js实现分开的，所以需要加载...已完成。");
     });
 }
+//聊天表单控件
+document.getElementById("chat-input").addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    //提交表单
+    e.value = "";
+  }
+});
+document.getElementById("chat-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const chatInput = document.getElementById("chat-input");
+  const chatSub = document.getElementById("chat-sub");
+  if (ws && ws.readyState === WebSocket.OPEN) {
+    chatInput.removeAttribute("disabled");
+    chatSub.innerText = "发送";
+  } else if (connectWS()) {
+    chatInput.removeAttribute("disabled");
+    chatSub.innerText = "发送";
+    console.log("连接成功");
+  }
+  let msg = chatInput.value;
+  sendWS(msg);
+  chatInput.value = "";
+  chatInput.setAttribute("rows", "1");
+  console.log("表单已经提交", msg);
+});
+// 连接websocket
+let ws;
 
+async function connectWS() {
+  ws = new WebSocket("ws://127.0.0.1:5000/ws");
+  ws.onmessage = function (event) {
+    let newMessage = document
+      .getElementsByClassName("other-message")[0]
+      .cloneNode(true);
+    newMessage.children[1].children[0].innerHTML = "对方";
+    newMessage.children[1].children[1].innerHTML = event.data;
+    document.getElementById("chat-content").appendChild(newMessage);
+  };
+  if (ws.readyState === WebSocket.OPEN) {
+    console.log("连接true",ws.readyState === WebSocket.OPEN);
+     return true;
+  } else {
+    console.log("连接false",ws.readyState === WebSocket.OPEN);
+    return false;
+  }
+}
+
+function sendWS(data) {
+  if (ws && ws.readyState === WebSocket.OPEN) {
+    ws.send(data);
+  }
+}
+//动画
 function rise() {
   let now = new Date();
   let hours = now.getHours();
   let min = now.getMinutes();
-  let t=hours.toString()+"."+min.toString();
+  let t = hours.toString() + "." + min.toString();
   let x = 160 * Math.sin(0.263 * t);
-  let y = 100 * Math.sin(0.263 * t - 23.5);
-  
-  if (6<t && t<18) {
-    console.log(6<t<18)
+  let y = 50 * Math.sin(0.263 * t - 23.5);
+
+  if (6 < t && t < 18) {
     document.getElementById(
       "avatar"
-    ).style.boxShadow = `${-x}px ${y}px 10px 0pxrgba(255, 32, 32, 0.3)`;
+    ).style.boxShadow = `${-x}px ${y}px 10px 0px rgba(255, 32, 32, 0.82)`;
   }
 
   //12点 x= 0，y= -100
   //6点  x= -200，y= 0
 }
-rise()
+rise();
--- js/index.js
+++ js/index.js
@@ -1,41 +1,17 @@
+window.addEventListener("resize", function () {});
 
-const urlParams = new URLSearchParams(window.location.search);
-
-
-// 获取所有参数名和值
-let query = "?";
-for (const [key, value] of urlParams.entries()) {
-  query = query + key + "=" + value + "&";
-}
-//删除最后一个&
-query = query.substring(0, query.length - 1);
-
-// 获取指定参数的所有值
-const paramValues = urlParams.getAll("paramName");
-
-// 判断是否登录-隐藏登录按钮
-function hiddenDiv() {
-  const hiddenDiv = document.getElementById("hidden-login");
-  hiddenDiv.classList.toggle("hidden");
-}
-
-// 登录按钮点击事件
-function login() {
-  fetch("http://127.0.0.1:8080/api/data/getdata" + query)
-    .then((response) => response.json())
-    .then((data) => {
-      // 处理返回的数据
-      console.log(query);
-      alert("登录成功" + JSON.stringify(data));
-    })
-    .catch((error) => {
-      // 处理错误
-      console.error(error);
-    });
-}
-// 搜索框回车事件
-function updateSearchAction() {
-  const searchEngine = document.getElementById('searchEngine').value;
-  const searchForm = document.getElementById('searchForm');
-  searchForm.action = searchEngine;
-}
+const textarea = document.getElementById("chat-input");
+textarea.addEventListener("input", function () {
+    this.setAttribute("rows", "1");
+    const lineCount = Math.floor(
+    textarea.scrollHeight /
+      parseInt(window.getComputedStyle(textarea).lineHeight, 10)
+  );
+  if (lineCount > 5) {
+    this.setAttribute("rows", 5);
+    return;
+  } else {
+    this.setAttribute("rows", lineCount);
+  }
+  let a = 1;
+});
